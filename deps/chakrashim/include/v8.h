04446e4648a464814436807419d6bbd778f62ef5
Kunal Pathak <Kunal.Pathak@microsoft.com>
2015-11-30 19:07:19 -0800

    chakrashim: Implemented ScriptOrigin signature

--- deps/chakrashim/include/v8.h
+++ deps/chakrashim/include/v8.h
@@ -771,11 +771,26 @@ class V8_EXPORT Data {
 
 class ScriptOrigin {
  public:
-  explicit ScriptOrigin(Handle<Value> resource_name)
-      : resource_name_(resource_name) {}
-  Handle<Value> ResourceName() const { return resource_name_; }
+  explicit ScriptOrigin(
+    Local<Value> resource_name,
+    Local<Integer> resource_line_offset = Local<Integer>(),
+    Local<Integer> resource_column_offset = Local<Integer>()) :
+      resource_name_(resource_name),
+      resource_line_offset_(resource_line_offset),
+      resource_column_offset_(resource_column_offset) {}
+  Handle<Value> ResourceName() const {
+    return resource_name_;
+  }
+  Local<Integer> ResourceLineOffset() const {
+    return resource_line_offset_;
+  }
+  Local<Integer> ResourceColumnOffset() const {
+    return resource_column_offset_;
+  }
  private:
   Handle<Value> resource_name_;
+  Local<Integer> resource_line_offset_;
+  Local<Integer> resource_column_offset_;
 };
 
 class V8_EXPORT UnboundScript {
