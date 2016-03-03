1cca549e0ad7313787518bfab9765d9143fae0ae
Sandeep Agarwal <agarwal.sandeep@microsoft.com>
2016-03-03 09:37:54 -0800

    test: fix unittest for Chakra engine

--- test/parallel/test-tls-external-accessor.js
+++ test/parallel/test-tls-external-accessor.js
@@ -13,12 +13,16 @@ const tls = require('tls');
 {
   const pctx = tls.createSecureContext().context;
   const cctx = Object.create(pctx);
-  assert.throws(() => cctx._external, /incompatible receiver/);
+  assert.throws(() => cctx._external, common.engineSpecificMessage({
+    v8 : /incompatible receiver/,
+    chakracore : /Illegal invocation/}));
   pctx._external;
 }
 {
   const pctx = tls.createSecurePair().credentials.context;
   const cctx = Object.create(pctx);
-  assert.throws(() => cctx._external, /incompatible receiver/);
+  assert.throws(() => cctx._external, common.engineSpecificMessage({
+    v8 : /incompatible receiver/,
+    chakracore : /Illegal invocation/}));
   pctx._external;
 }
