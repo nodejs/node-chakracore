53c2d8e8e18f0442b183ce769219bcedec175373
Jianchun Xu <jianxu@microsoft.com>
2016-04-06 15:18:26 -0700

    test: skip null-buffer-neuter on node-chakracore
    null-buffer-neuter tests GC behavior and doesn't work on node-chakracore.
    
    PR-URL: https://github.com/nodejs/node-chakracore/pull/52
    Reviewed-By: Sandeep Agarwal <Agarwal.Sandeep@microsoft.com>

--- test/addons/null-buffer-neuter/test.js
+++ test/addons/null-buffer-neuter/test.js
@@ -1,7 +1,14 @@
 'use strict';
 // Flags: --expose-gc
 
-require('../../common');
+const common = require('../../common');
 var binding = require('./build/Release/binding');
+
+if (common.isChakraEngine) {
+  console.log(`1..0 # Skipped: This test is disabled for chakra engine
+  because it depends on v8 GC behavior. Chakra GC may find reference-like
+  data on native stack and may not do the GC as expected.`);
+  return;
+}
 
 binding.run();
