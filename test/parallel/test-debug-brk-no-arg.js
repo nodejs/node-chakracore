b3610fe8f4061c9eada1d0e3f6be3824593e6720
Kunal Pathak <Kunal.Pathak@microsoft.com>
2016-06-14 11:34:03 -0700

    test: fixed unit test for error message
    With this there is one extra failure in `parallel/test-repl` that
    I will investigate later.
    
    PR-URL: nodejs/node-chakracore#79
    Reviewed-By: Jianchun Xu <Jianchun.Xu@microsoft.com>

--- test/parallel/test-debug-brk-no-arg.js
+++ test/parallel/test-debug-brk-no-arg.js
@@ -3,6 +3,12 @@ const common = require('../common');
 const assert = require('assert');
 const spawn = require('child_process').spawn;
 
+if (common.isChakraEngine) {
+  console.log('1..0 # Skipped: This test is disabled for chakra engine ' +
+  'because debugger support is not implemented yet.');
+  return;
+}
+
 const child = spawn(process.execPath, ['--debug-brk=' + common.PORT, '-i']);
 child.stderr.once('data', common.mustCall(function() {
   child.stdin.end('.exit');
