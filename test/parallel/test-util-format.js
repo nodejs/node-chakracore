c392c6bad4c1284c43010f459862359a01a4aa58
Kunal Pathak <Kunal.Pathak@microsoft.com>
2017-03-15 15:27:33 -0700

    Merge commit 'xplat' into xplat-v8.x

--- test/parallel/test-util-format.js
+++ test/parallel/test-util-format.js
@@ -20,7 +20,7 @@
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 'use strict';
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const util = require('util');
 const symbol = Symbol('foo');
@@ -83,7 +83,18 @@ assert.strictEqual(util.format('o: %j, a: %j'), 'o: %j, a: %j');
 {
   const o = {};
   o.o = o;
-  assert.strictEqual(util.format('%j', o), '[Circular]');
+  if (!common.isChakraEngine) {
+    assert.strictEqual(util.format('%j', o), '[Circular]');
+  } else {
+    assert.throws(() => { util.format('%j', o); }, function(err) {
+      if (err instanceof TypeError &&
+          err.message === 'Circular reference in value argument not ' +
+                          'supported') {
+        return true;
+      }
+      return false;
+    });
+  }
 }
 
 {
