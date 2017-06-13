nodejs/node-chakracore

commit df8e7cab7d4a16f8c6d2cb285a3b9d2dc89b54b3

build,chakrashim: enabled cpplint for chakrashim
* Added support for the `cpplint` argument
* Added support for chakrashim to cpplint
* Updated cpplint filters to match the beginning of the line
* Updated cpplint filters to echo to nul as the message was misleading
* Fixed all cpplint failures in `deps/chakrashim/src`

PR-URL: https://github.com/nodejs/node-chakracore/pull/287
Reviewed-By: Sandeep Agarwal <saagarwa@microsoft.com>

deps/chakrashim/src/v8regexp.cc
@@ -25,14 +25,15 @@ namespace v8 {
 using jsrt::IsolateShim;
 using jsrt::ContextShim;
 
-static const size_t c_maxFlagCount = 3;
+static const size_t kMaxFlagCount = 3;
 
 MaybeLocal<RegExp> RegExp::New(Local<Context> context, Handle<String> pattern,
                                Flags flags) {
-  ContextShim *contextShim = IsolateShim::GetContextShim((JsContextRef)*context);
+  ContextShim *contextShim = IsolateShim::GetContextShim(
+      (JsContextRef)*context);
   JsValueRef regExpConstructor = contextShim->GetRegExpConstructor();
 
-  char flagsStr[c_maxFlagCount];
+  char flagsStr[kMaxFlagCount];
   size_t flagsCount = 0;
 
   if (flags & Flags::kGlobal) {
@@ -55,7 +56,8 @@ MaybeLocal<RegExp> RegExp::New(Local<Context> context, Handle<String> pattern,
   }
 
   JsValueRef newObj;
-  if (jsrt::ConstructObject(regExpConstructor, (JsValueRef)*pattern, flagsValue, &newObj) != JsNoError) {
+  if (jsrt::ConstructObject(regExpConstructor, (JsValueRef)*pattern, flagsValue,
+                            &newObj) != JsNoError) {
     return MaybeLocal<RegExp>();
   }
 

commit e1fc8c0621b3080d845ead428d472bd3ffa554ef

chakrashim: fix debug build failure
Fixing debug build failure introduced in
df8e7cab7d4a16f8c6d2cb285a3b9d2dc89b54b3

PR-URL: https://github.com/nodejs/node-chakracore/pull/298
Reviewed-By: Kunal Pathak <kunal.pathak@microsoft.com>
Reviewed-By: Hitesh Kanwathirtha <hiteshk@microsoft.com>

deps/chakrashim/src/v8regexp.cc
@@ -48,7 +48,7 @@ MaybeLocal<RegExp> RegExp::New(Local<Context> context, Handle<String> pattern,
     flagsStr[flagsCount++] = 'm';
   }
 
-  CHAKRA_ASSERT(flagsCount <= c_maxFlagCount);
+  CHAKRA_ASSERT(flagsCount <= kMaxFlagCount);
 
   JsValueRef flagsValue;
   if (JsCreateString(flagsStr, flagsCount, &flagsValue) != JsNoError) {