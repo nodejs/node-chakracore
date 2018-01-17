46c97a95c2f34b710daad3189a98d494f78f72d4
Taylor Woll <tawoll@ntdev.microsoft.com>
2018-01-16 20:26:45 -0800

    fix lint issue

--- test/addons-napi/test_new_target/test.js
+++ test/addons-napi/test_new_target/test.js
@@ -16,5 +16,6 @@ class Class extends binding.BaseClass {
 
 assert.ok(new Class() instanceof binding.BaseClass);
 assert.ok(new Class().ok);
-assert.ok(new binding.Constructor(binding.Constructor) instanceof binding.Constructor);
 assert.ok(binding.OrdinaryFunction());
+assert.ok(
+  new binding.Constructor(binding.Constructor) instanceof binding.Constructor);
