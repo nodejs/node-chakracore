5de2f8b4f68f7554d65208e576d1615eb2685b6f
Jack Horton <jahorto@microsoft.com>
2018-02-03 16:47:50 -0800

    lint: fix lint issues from ttd fixes

--- src/node.cc
+++ src/node.cc
@@ -4198,8 +4198,7 @@ static void ParseArgs(int* argc,
       // Parse and extract the TT args
       } else if (strcmp(arg, "--record") == 0) {
           s_doTTRecord = true;
-      }
-      else if (strstr(arg, "--tt-debug") == arg) {
+      } else if (strstr(arg, "--tt-debug") == arg) {
         s_doTTRecord = true;
         s_doTTEnableDebug = true;
         s_ttdSnapInterval = 500;
@@ -5130,7 +5129,8 @@ inline int Start_TTDReplay(uv_loop_t* event_loop,
   Isolate* const isolate = Isolate::NewWithTTDSupport(params,
                                                       s_ttoptReplayUriLength,
                                                       s_ttoptReplayUri,
-                                                      false, true, s_doTTEnableDebug,
+                                                      false, true,
+                                                      s_doTTEnableDebug,
                                                       UINT32_MAX, UINT32_MAX);
 
   if (isolate == nullptr)
