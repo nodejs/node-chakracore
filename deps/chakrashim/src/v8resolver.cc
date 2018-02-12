354e3064aa262fa7ed3a4906b4eb1e51b1df46ae
Jimmy Thomson <jithomso@microsoft.com>
2018-02-12 15:39:48 -0800

    shim: adding v8::Promise and v8::Promise::Resolver implementations

--- deps/chakrashim/src/v8resolver.cc
+++ deps/chakrashim/src/v8resolver.cc
@@ -25,7 +25,35 @@ namespace v8 {
   using Resolver = Promise::Resolver;
   // CHAKRA-TODO: Unimplemented completely
   MaybeLocal<Resolver> Resolver::New(Local<Context> context) {
-    return Local<Resolver>();
+    JsValueRef resolver;
+    if (JsCreateObject(&resolver) != JsNoError) {
+      return Local<Resolver>();
+    }
+
+    JsValueRef promise, resolve, reject;
+    if (JsCreatePromise(&promise, &resolve, &reject) != JsNoError) {
+      return Local<Resolver>();
+    }
+
+    if (jsrt::SetProperty(resolver, jsrt::CachedPropertyIdRef::promise, promise) != JsNoError) {
+      return Local<Resolver>();
+    }
+
+    if (jsrt::SetProperty(resolver, jsrt::CachedPropertyIdRef::resolve, resolve) != JsNoError) {
+      return Local<Resolver>();
+    }
+
+    if (jsrt::SetProperty(resolver, jsrt::CachedPropertyIdRef::reject, reject) != JsNoError) {
+      return Local<Resolver>();
+    }
+
+    JsValueRef state;
+    jsrt::UintToValue(0, &state);
+    if (jsrt::SetProperty(promise, jsrt::CachedPropertyIdRef::value, state) != JsNoError) {
+      return Local<Resolver>();
+    }
+
+    return Local<Resolver>::New(static_cast<Resolver*>(resolver));
   }
 
   Local<Resolver> Resolver::New(Isolate* isolate) {
@@ -33,8 +61,11 @@ namespace v8 {
   }
 
   Local<Promise> Resolver::GetPromise() {
-    CHAKRA_ASSERT(false);
-    return Local<Promise>();
+    JsValueRef promise;
+    if (jsrt::GetProperty(this, jsrt::CachedPropertyIdRef::promise, &promise) != JsNoError) {
+      return Local<Promise>();
+    }
+    return Local<Promise>::New(static_cast<Promise*>(promise));
   }
 
   Resolver* Resolver::Cast(Value* obj) {
@@ -42,7 +73,36 @@ namespace v8 {
   }
 
   Maybe<bool> Resolver::Resolve(Local<Context> context, Local<Value> value) {
-    return Just(false);
+    JsValueRef resolve;
+    if (jsrt::GetProperty(this, jsrt::CachedPropertyIdRef::resolve, &resolve) != JsNoError) {
+      return Nothing<bool>();
+    }
+
+    JsValueRef promise;
+    if (jsrt::GetProperty(this, jsrt::CachedPropertyIdRef::promise, &promise) != JsNoError) {
+      return Nothing<bool>();
+    }
+
+    if (jsrt::SetProperty(promise, jsrt::CachedPropertyIdRef::value, *value) != JsNoError) {
+      return Nothing<bool>();
+    }
+
+    JsValueRef state;
+    jsrt::UintToValue(1, &state);
+    if (jsrt::SetProperty(promise, jsrt::CachedPropertyIdRef::value, state) != JsNoError) {
+      return Nothing<bool>();
+    }
+    
+    JsValueRef result;
+    JsValueRef args[2];
+    args[0] = this; // ? What is the "this" of the resolver here?
+    args[1] = reinterpret_cast<JsValueRef>(*value);
+
+    if (JsCallFunction(resolve, args, 2, &result) != JsNoError) {
+      return Nothing<bool>();
+    }
+
+    return Just(true);
   }
 
   void Resolver::Resolve(Local<Value> value) {
@@ -51,6 +111,35 @@ namespace v8 {
   }
 
   Maybe<bool> Resolver::Reject(Local<Context> context, Local<Value> value) {
-    return Just(false);
+    JsValueRef reject;
+    if (jsrt::GetProperty(this, jsrt::CachedPropertyIdRef::reject, &reject) != JsNoError) {
+      return Nothing<bool>();
+    }
+
+    JsValueRef promise;
+    if (jsrt::GetProperty(this, jsrt::CachedPropertyIdRef::promise, &promise) != JsNoError) {
+      return Nothing<bool>();
+    }
+
+    if (jsrt::SetProperty(promise, jsrt::CachedPropertyIdRef::value, *value) != JsNoError) {
+      return Nothing<bool>();
+    }
+
+    JsValueRef state;
+    jsrt::UintToValue(2, &state);
+    if (jsrt::SetProperty(promise, jsrt::CachedPropertyIdRef::value, state) != JsNoError) {
+      return Nothing<bool>();
+    }
+    
+    JsValueRef result;
+    JsValueRef args[2];
+    args[0] = this; // ? What is the "this" of the resolver here?
+    args[1] = reinterpret_cast<JsValueRef>(*value);
+
+    if (JsCallFunction(reject, args, 2, &result) != JsNoError) {
+      return Nothing<bool>();
+    }
+
+    return Just(true);
   }
 }  // namespace v8
