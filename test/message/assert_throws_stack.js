857281b55283550f5d7d859750466c6e83deebe8
Mike Kaufman <mike-kaufman@users.noreply.github.com>
2018-03-28 13:30:30 -0700

    test: re-enable assert_throws_stack tests for node-chakracore (#504)

--- test/message/assert_throws_stack.js
+++ test/message/assert_throws_stack.js
@@ -3,8 +3,4 @@
 require('../common');
 const assert = require('assert').strict;
 
-// Temporarily disabled on chakra.
-const common = require('../common');
-if (!common.isChakraEngine) {
 assert.throws(() => { throw new Error('foo'); }, { bar: true });
-}
