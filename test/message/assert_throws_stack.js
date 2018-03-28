4e8b529fc4dc6907baaf14090f3e69efeb9287dc
Mike Kaufman <mkaufman@microsoft.com>
2018-03-28 11:55:16 -0700

    disabling failing test

--- test/message/assert_throws_stack.js
+++ test/message/assert_throws_stack.js
@@ -3,4 +3,7 @@
 require('../common');
 const assert = require('assert').strict;
 
+// Temporarily disabled on chakra.
+if (!common.isChakraEngine) {
 assert.throws(() => { throw new Error('foo'); }, { bar: true });
+}
