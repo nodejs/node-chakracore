6e878e949fd523291f9518dd79ae3aa22870a134
Jaime Bernardo <jaime@janeasystems.com>
2018-05-23 14:15:36 +0100

    test,ios: use same timeout in proxy as test.py
    Use the same timeout for calling ios-deploy as the one defined in
    tools/test.py, for tests that take longer but are still within the
    expected timeout value.

--- tools/mobile-test/ios/node-ios-proxy.sh
+++ tools/mobile-test/ios/node-ios-proxy.sh
@@ -16,6 +16,6 @@ TEST_BASE_DIR="$( cd "$( dirname "$0" )" && cd .. && cd .. && cd test && pwd )"
 
 echo "Time: $(date '+%FT%T') -> Proxying testcase: $TEMP_COMMAND" >> "$LOG_FILE_PATH"
 
-ios-deploy -t 60 --noinstall $TARGET_DEVICE -b "$IOS_APP_PATH" --noninteractive --args "--substitute-dir $TEST_BASE_DIR $*" | sed $'s/\r$//' | tee -a "$LOG_FILE_PATH" | sed '1,/(lldb)     autoexit/d' | sed -E '/Process [0-9]+ exited with status.*|PROCESS_EXITED/,$d'
+ios-deploy -t 120 --noinstall $TARGET_DEVICE -b "$IOS_APP_PATH" --noninteractive --args "--substitute-dir $TEST_BASE_DIR $*" | sed $'s/\r$//' | tee -a "$LOG_FILE_PATH" | sed '1,/(lldb)     autoexit/d' | sed -E '/Process [0-9]+ exited with status.*|PROCESS_EXITED/,$d'
 
 exit ${PIPESTATUS[0]}
