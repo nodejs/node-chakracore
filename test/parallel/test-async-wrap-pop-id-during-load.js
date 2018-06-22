6b87682db2b9ec14b7ec9b9b854dfcdacb891051
Kyle Farnung <kfarnung@microsoft.com>
2018-06-22 16:57:09 -0700

    test: fix/skip tests for ChakraCore
    * Fixed test-async-wrap-pop-id-during-load to run correctly
    * Skipped test-assert-builtins-not-read-from-filesystem since it's
      testing the assert message behavior we don't support anyway.
    
    PR-URL: https://github.com/nodejs/node-chakracore/pull/556
    Reviewed-By: Jimmy Thomson <jithomso@microsoft.com>

--- test/parallel/test-async-wrap-pop-id-during-load.js
+++ test/parallel/test-async-wrap-pop-id-during-load.js
@@ -13,17 +13,17 @@ if (process.argv[2] === 'async') {
 const assert = require('assert');
 const { spawnSync } = require('child_process');
 
+const engineArgs = [];
+if (!common.isChakraEngine) {
+  engineArgs.push('--stack_size=75');
+}
+
 const ret = spawnSync(
   process.execPath,
-  [common.engineSpecificMessage({
-    v8: '--stack_size=75',
-    chakracore: ''
-  }), __filename, 'async']
+  [...engineArgs, __filename, 'async']
 );
 assert.strictEqual(ret.status, 0,
                    `EXIT CODE: ${ret.status}, STDERR:\n${ret.stderr}`);
 const stderr = ret.stderr.toString('utf8', 0, 2048);
 assert.ok(!/async.*hook/i.test(stderr));
-if (!common.isChakraEngine) {
-  assert.ok(stderr.includes('UnhandledPromiseRejectionWarning: Error'), stderr);
-}
+assert.ok(stderr.includes('UnhandledPromiseRejectionWarning: Error'), stderr);
