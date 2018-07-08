fb175591436e9fd7f2687fd31da6a8056fdc9385
Kyle Farnung <kfarnung@microsoft.com>
2018-07-07 19:02:39 -0700

    test: update Symbol error message
    PR-URL: https://github.com/nodejs/node-chakracore/pull/568
    Reviewed-By: Jimmy Thomson <jithomso@microsoft.com>

--- test/parallel/test-whatwg-url-searchparams-append.js
+++ test/parallel/test-whatwg-url-searchparams-append.js
@@ -72,18 +72,16 @@ test(function() {
     valueOf() { throw new Error('valueOf'); }
   };
   const sym = Symbol();
-  const chakracoreSymbolErrorRegex =
-         /^TypeError: Object doesn't support property or method 'ToString'/;
   assert.throws(() => params.set(obj, 'b'), /^Error: toString$/);
   assert.throws(() => params.set('a', obj), /^Error: toString$/);
   assert.throws(() => params.set(sym, 'b'),
                 common.engineSpecificMessage({
                   v8: /^TypeError: Cannot convert a Symbol value to a string$/,
-                  chakracore: chakracoreSymbolErrorRegex
+                  chakracore: /^TypeError: No implicit conversion of Symbol to String/
                 }));
   assert.throws(() => params.set('a', sym),
                 common.engineSpecificMessage({
                   v8: /^TypeError: Cannot convert a Symbol value to a string$/,
-                  chakracore: chakracoreSymbolErrorRegex
+                  chakracore: /^TypeError: No implicit conversion of Symbol to String/
                 }));
 }
--- test/parallel/test-whatwg-url-searchparams-has.js
+++ test/parallel/test-whatwg-url-searchparams-has.js
@@ -60,12 +60,10 @@ test(function() {
     valueOf() { throw new Error('valueOf'); }
   };
   const sym = Symbol();
-  const chakracoreSymbolErrorRegex =
-         /^TypeError: Object doesn't support property or method 'ToString'/;
   assert.throws(() => params.has(obj), /^Error: toString$/);
   assert.throws(() => params.has(sym),
                 common.engineSpecificMessage({
                   v8: /^TypeError: Cannot convert a Symbol value to a string$/,
-                  chakracore: chakracoreSymbolErrorRegex
+                  chakracore: /^TypeError: No implicit conversion of Symbol to String/
                 }));
 }
