06ff556f9eb0c1c926b317373f17c8c12d8c08b8
Jimmy Thomson <jithomso@microsoft.com>
2018-08-17 15:20:47 -0700

    test: disabling unsupported parts of tests

--- test/parallel/test-util-inspect.js
+++ test/parallel/test-util-inspect.js
@@ -1665,8 +1665,12 @@ assert.strictEqual(util.inspect('"\'${a}'), "'\"\\'${a}'");
   assert.notStrictEqual(util.inspect(value), expected);
 });
 
+if (!common.isChakraEngine) {
+  eval(`
 assert.strictEqual(inspect(1n), '1n');
 assert.strictEqual(inspect(Object(-1n)), '[BigInt: -1n]');
 assert.strictEqual(inspect(Object(13n)), '[BigInt: 13n]');
 assert.strictEqual(inspect(new BigInt64Array([0n])), 'BigInt64Array [ 0n ]');
 assert.strictEqual(inspect(new BigUint64Array([0n])), 'BigUint64Array [ 0n ]');
+  `);
+}
